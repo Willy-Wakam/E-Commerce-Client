54a43600cc8cea0ff6fc41ca279855c4f02d8a6e
Willy Wakam <williamswakam09@gmail.com>
2025-06-19 14:47:41 +0200

    Add routes for login & register authentification for admin or customers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,31 +12,51 @@ import ShoppingHome from './pages/shopping-view/Home'
 import ShoppingAccount from './pages/shopping-view/Account'
 import ShoppingCardCheckout from './pages/shopping-view/CheckOut'
 import ShoppingProductsList from './pages/shopping-view/ProductsList'
+import CheckAuth from './components/common/Check-Auth'
+import UnAuthPage from './pages/un-auth/UnAuthPage'
 
 function App() {
+
+  const isAuthenticated = true; // Replace with actual authentication logic
+  const user = {
+    role: 'customer', // or 'admin' based on the logged-in user
+  }; // Replace with actual user data
 
   return (
     <>
       <div className='flex flex-col overflow-hidden bg-white'>
 
         <Routes>
-          <Route path='/auth' element={<AuthLayout />}>
+          <Route path='/auth' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }>
             <Route path='login' element={<Login />} />
             <Route path='register' element={<Register />} />
           </Route>
-          <Route path='/admin' element={<AdminViewLayout />}>
+          <Route path='/admin' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminViewLayout />
+            </CheckAuth>
+          }>
             <Route path='dashboard' element={<AdminDashboard />} />
             <Route path='products' element={<AdminProducts />} />
             <Route path='features' element={<AdminFeatures />} />
             <Route path='orders' element={<AdminOrders />} />
           </Route>
-          <Route path='/shop' element={<ShoppingLayout />}>
+          <Route path='/shop' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }>
             <Route path='home' element={<ShoppingHome />} />
             <Route path='account' element={<ShoppingAccount />} />
             <Route path='checkout' element={<ShoppingCardCheckout/>} />
             <Route path='products' element={<ShoppingProductsList />} />
           </Route>
           <Route path='*' element={<h1 className='text-3xl text-center'>Page Not Found</h1>} />
+          <Route path='/unauth-page' element={<UnAuthPage />} />
         </Routes>
       </div>
     </>

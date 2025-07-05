634aabf8c607fbc1050700e46471d9e183ab9171
Willy Wakam <williamswakam09@gmail.com>
2025-07-05 18:05:31 +0200

    Debugging

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -3,7 +3,7 @@ import CommonForm from "@/components/common/Form";
 import { loginControls } from "@/config";
 import { loginUser } from "@/store/auth-slice";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -13,6 +13,7 @@ const initialState = {
 };
 function Login() {
  const [formData, setFormData] = useState(initialState);
+ const {token} = useSelector((state) => state.auth)
 
  const dispatch = useDispatch();
 
@@ -25,6 +26,7 @@ function Login() {
         toast.error(response?.payload?.message || "Login failed. Please try again.");
       } 
     });
+    sessionStorage.setItem("token", token);
   }
 
   return (

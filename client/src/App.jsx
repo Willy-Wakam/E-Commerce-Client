8dc2cdfd1cd3ecefd70df854f12101fae297a8d9
Willy Wakam <williamswakam09@gmail.com>
2025-07-06 21:20:38 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import AuthLayout from "./components/auth/Layout";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";

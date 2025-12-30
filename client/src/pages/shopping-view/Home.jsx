cb75307bbd2aea202a3fb810bd02ba5d21b6f270
Willy Wakam <williamswakam09@gmail.com>
2025-12-30 11:19:41 +0100

    Add useNavigate hook to ShoppingHome component

--- client/src/pages/shopping-view/Home.jsx
+++ client/src/pages/shopping-view/Home.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 function ShoppingHome() {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Welcome to the Shopping App</h1>
